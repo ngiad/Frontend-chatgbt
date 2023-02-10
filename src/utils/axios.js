ec8a7e95099b5141cc502216fce75e9b385468b2
ngiad <follownghia001@gmail.com>
2023-02-10 14:11:57 +0700

    4

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const Requestbase = axios.create({
-    baseURL : "http://localhost:5000/"
+    baseURL : "https://backend-chatgbt.onrender.com/"
 })
 
 export default Requestbase
